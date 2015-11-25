48ebfd8449949a7fd8ab001216a0c958bc687902
Mayank Yadav <Mayank.yadav@yash.com>
2015-11-25 15:19:45 +0530

    First commit

---
+++ TraininigManagamentSystem/src/com/yash/training/tmp/service/SubheadingBeanServiceLocal.java
@@ -0,0 +1,15 @@
+package com.yash.training.tmp.service;
+
+import javax.ejb.Local;
+
+import com.yash.training.tmp.domain.Subheading;
+import com.yash.training.tmp.domain.User;
+
+@Local
+public interface SubheadingBeanServiceLocal {
+
+	public int getHeadingid(User user,String heading_text);
+	public void saveSubheadingService(Subheading subheading);
+	
+	
+}
